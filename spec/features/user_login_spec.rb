cd94a037e72c4d662daf1cb668579ef0093c484e
Dan Broadbent <dbroadbent85@gmail.com>
2017-01-09 09:57:41 -0700

    Add sad path sign up tests and logout test

--- spec/features/user_login_spec.rb
+++ spec/features/user_login_spec.rb
@@ -9,8 +9,9 @@ RSpec.describe "User login" do
   end
 
   scenario "Unauthenticated user logs in" do
+    User.new({email: 'joe@example.com', password: 'password'})
     visit "/"
-    
+
     expect(page).not_to have_text("Logout")
 
     click_on "Log In"
@@ -19,6 +20,22 @@ RSpec.describe "User login" do
     click_on "Submit"
 
     expect(page).to have_text("Logout")
+    expect(page).to have_text("Succesfully logged in")
     expect(current_path).to eq('/')
+  end
+
+  scenario "Email doesn't match password" do
+    User.new({email: 'joe@example.com', password: 'password'})
+    visit "/"
+
+    expect(page).not_to have_text("Logout")
+
+    click_on "Log In"
+    fill_in "Email:", with: "joe@example.com"
+    fill_in "Password:", with: "wordpass"
+    click_on "Submit"
+
+    expect(page).to have_text("Email and password do not match, please try again")
+    expect(current_path).to eq('/login')
   end
 end
