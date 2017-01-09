313e0713ab5153008610cac753bf033e450057e6
Dan Broadbent <dbroadbent85@gmail.com>
2017-01-09 13:26:04 -0700

    Fix edit link test

--- spec/features/edit_link_spec.rb
+++ spec/features/edit_link_spec.rb
@@ -1,38 +1,26 @@
-# require "rails_helper"
-#
-# RSpec.describe "Can edit links" do
-#   scenario "edit a new link" do
-#     Link.create({title: 'Turing', url: 'http://turing.io'})
-#     login_user
-#     visit "/"
-#     click_on "Edit"
-#
-#     expect(current_path).to eq('/links/1/edit')
-#
-#     fill_in "Title", with: "Turing 2"
-#     fill_in "URL", with: "http://turing.io/2"
-#     click_on "Submit"
-#
-#     within('#links-list') do
-#       expect(page).to have_text("Turing 2")
-#       expect(page).to have_text("http://turing.io/2")
-#     end
-#   end
-#
-#   scenario "Invalid link" do
-#     login_user
-#     visit "/"
-#     fill_in "Title:", :with => "Turing"
-#     fill_in "URL:", :with => "http://turing.io"
-#     click_on "Add Link"
-#     click_on "Edit"
-#
-#     expect(current_path).to eq('/links/1/edit')
-#
-#     fill_in "Title", with: "Turing 2"
-#     fill_in "URL", with: "turing 2"
-#     click_on "Submit"
-#
-#     expect(page).to have_text("Url is not a valid URL")
-#   end
-# end
+require "rails_helper"
+
+RSpec.describe "Can edit links" do
+  scenario "edit a new link" do
+    Link.create({title: 'Turing', url: 'http://turing.io'})
+    login_user
+    visit '/links/1/edit'
+    fill_in "link[title]", with: "Turing 2"
+    fill_in "link[url]", with: "http://turing.io/2"
+    click_on "Submit"
+
+    expect(Link.last.title).to eq("Turing 2")
+    expect(Link.last.url).to eq("http://turing.io/2")
+  end
+
+  scenario "Invalid link" do
+    Link.create({title: 'Turing', url: 'http://turing.io', id: 2})
+    login_user
+    visit '/links/2/edit'
+    fill_in "link[title]", with: "Turing 2"
+    fill_in "link[url]", with: "turing 2"
+    click_on "Submit"
+
+    expect(page).to have_text("Url is not a valid URL")
+  end
+end
